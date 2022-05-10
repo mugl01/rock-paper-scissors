e8e8b18ab5c72ddadc58c788e64b49dc1d33b41e
Gonzalez Maroto <mugl@gft.com>
2022-05-10 10:01:29 +0200

    Added navigation

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  navigateToHome(){
+    this.router.navigate(['home']);
   }
 
 }
