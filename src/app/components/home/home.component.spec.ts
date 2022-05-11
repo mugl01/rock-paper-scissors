5fea2979411d17e214c490d398a3bb436b18f377
Gonzalez Maroto <mugl@gft.com>
2022-05-11 17:14:22 +0200

    added tests

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -3,10 +3,14 @@ import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FormsModule, ReactiveFormsModule, NgForm } from '@angular/forms'
+import { Router } from '@angular/router';
+
+import { RockPaperScissorsService } from '../../services/rock-paper-scissors.service';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -24,6 +28,7 @@ describe('HomeComponent', () => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.inject(Router);
   });
 
   it('should create', () => {
@@ -42,13 +47,31 @@ describe('HomeComponent', () => {
   it('Return when form is invalid', () => {
     const player = <NgForm>{
       value: {
-          name: "Hello",
+          playerName: "Hello",
           category: "World",
       },
       invalid: true
-  };
+    };
     const resp = component.navigateToGame(player);
     expect(resp).toBeFalsy();
+  });
+
+  it('should navigate to game', () => {
+    const player = <NgForm>{
+      value: {
+          playerName: "Hello",
+          category: "World",
+      },
+      invalid: false
+    };
+
+    const component = fixture.componentInstance;
+    const navigateSpy = spyOn(router, 'navigate');
+    let service = fixture.debugElement.injector.get(RockPaperScissorsService);
+    spyOn(service, 'getComputerSelection');
+    component.navigateToGame(player);
+
+    expect(navigateSpy).toHaveBeenCalledWith(['game', player.value.playerName]);
   });
   
 });
