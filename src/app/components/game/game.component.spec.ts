74412f9f9a3ec95b105f937f91d2f7008ff8cc0d
Gonzalez Maroto <mugl@gft.com>
2022-05-11 15:35:43 +0200

    Added tests

--- src/app/components/game/game.component.spec.ts
+++ src/app/components/game/game.component.spec.ts
@@ -1,4 +1,5 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { GameComponent } from './game.component';
 
@@ -8,6 +9,7 @@ describe('GameComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ GameComponent ]
     })
     .compileComponents();
@@ -22,4 +24,31 @@ describe('GameComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('reset variables when calling resetScore', ()=> {
+    component.score = 10;
+    component.resetScore();
+    expect(component.score).toEqual(0);
+  });
+
+  it('should increment player score when player win', () => {
+    component.score = 1;
+    const userWon = 'User won';
+    component.setScore(userWon);
+    expect(component.score).toEqual(2);
+  });
+
+  it('set player selection', () => {
+    component.setPlayerSelection('paper');
+    expect(component.result).toEqual('User won');
+  });
+
+  it('Call reset score when click on button', async(() => {
+    spyOn(component,'resetScore');
+    let button = fixture.debugElement.nativeElement.querySelector('article>button');
+    button.click();
+    fixture.whenStable().then(() => {
+      expect(component.resetScore).toHaveBeenCalled();
+    });
+  }));
 });
--- src/app/services/rock-paper-scissors.service.spec.ts
+++ src/app/services/rock-paper-scissors.service.spec.ts
@@ -13,4 +13,57 @@ describe('RockPaperScissorsService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return a string when calling getComputerSelection', ()=>{
+    const resp = service.getComputerSelection();
+    expect(typeof resp ).toBe('string');
+  });
+
+  it('should return a string when calling playGame', () => {
+    const playerSelection = 'paper';
+    const resp = service.playGame(playerSelection);
+    expect(typeof resp).toBe('string');
+  });
+
+  it('should return tie when calling playGame', () => {
+    const playerSelection = 'paper';
+    service.computerSelection = 'paper';
+    const resp = service.playGame(playerSelection);
+    expect(resp).toEqual('Tie');
+  });
+
+  it('should return computer won  when calling playGame computer with paper and user with rock', () => {
+    const playerSelection = 'rock';
+    service.computerSelection = 'paper';
+    const resp = service.playGame(playerSelection);
+    expect(resp).toEqual('Computer won');
+  });
+
+  it('should return user won  when calling playGame computer with scissor and user with rock', () => {
+    const playerSelection = 'rock';
+    service.computerSelection = 'scissors';
+    const resp = service.playGame(playerSelection);
+    expect(resp).toEqual('User won');
+  });
+
+  it('should return computer won  when calling playGame computer with scissors and user with paper', () => {
+    const playerSelection = 'paper';
+    service.computerSelection = 'scissors';
+    const resp = service.playGame(playerSelection);
+    expect(resp).toEqual('Computer won');
+  });
+
+  it('should return computer won  when calling playGame computer with rock and user with scissors', () => {
+    const playerSelection = 'scissors';
+    service.computerSelection = 'rock';
+    const resp = service.playGame(playerSelection);
+    expect(resp).toEqual('Computer won');
+  });
+
+  it('should return user won  when calling playGame computer with paper and user with scissors', () => {
+    const playerSelection = 'scissors';
+    service.computerSelection = 'paper';
+    const resp = service.playGame(playerSelection);
+    expect(resp).toEqual('User won');
+  });
 });
